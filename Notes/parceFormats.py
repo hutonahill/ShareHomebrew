92b5c7dd5925b1523ee950baeca77dd84821f8cc
hutonahill <eriker@byui.edu>
2023-10-20 21:54:04 -0600

    slight changes

--- Notes/parceFormats.py
+++ Notes/parceFormats.py
@@ -28,15 +28,31 @@ def save_to_file(output_dict, output_file):
     with open(output_file, 'w') as file:
         json.dump(output_dict, file, indent=4)
 
+def extract_types(output_dict):
+    types_list = set()  # Using a set to ensure unique types
+
+    for page, monsters in output_dict.items():
+        for monster, attributes in monsters.items():
+            if "type" in attributes:
+                types_list.add(attributes["type"])
+
+    return list(types_list)
+
 def main():
     input_file = 'Monster page outline.txt'
-    output_file = 'output.json'
+    output_file = 'monsterFormat.json'
     
     script_dir = os.path.dirname(os.path.realpath(__file__))
     os.chdir(script_dir)
     
-    output = parse_text_file(input_file)
-    save_to_file(output, output_file)
+    #output = parse_text_file(input_file)
+    #save_to_file(output, output_file)
+    
+    with open(output_file, 'r') as file:
+        output_dict = json.load(file)
+
+    types_list = extract_types(output_dict)
+    print("List of Types:", types_list)
 
 if __name__ == "__main__":
     main()
