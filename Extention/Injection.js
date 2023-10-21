8e9675902e5117390926b7e8152326210e3b5eb2
hutonahill <eriker@byui.edu>
2023-10-21 06:04:25 -0600

    reading and writting strings now works. finaly.

--- Extention/Injection.js
+++ Extention/Injection.js
@@ -1,6 +1,98 @@
+function elementExists(id) {
+  return document.getElementById(id) !== null;
+}
+  class StringElement{
+    constructor(idInput, nameInput) {
+      /**
+       * The value associated with the HTML element.
+       * @type {string}
+       */
+      this.value = "";
+
+      this.type = "String"
+  
+      /**
+       * The Id the HTML element.
+       * @property {string} value
+       */
+      this.id = idInput;
+        
+      /**
+       * The name of the element.
+       * @type {string}
+       */
+      this.name = nameInput
+    }
+  
+    /**
+     * Retrieves the value from the current HTML page.
+     *
+     * @method
+     * @returns {void}
+     */
+    readValue() {
+        console.log("Starting Read Test")
+        let element = document.getElementById(this.id);
+  
+  
+        if (element) {
+          console.log("element.value: ", element.value);
+          console.log("element: ", element)
+
+            this.value = element.value;
+        } else {
+            console.error(`Element with id '${this.id}' not found.`);
+        }
+    }
+  
+    writeValue(){
+      console.log("Starting Write Test")
+      console.log("id: ", this.id);
+      let element = document.getElementById(this.id);
+  
+      if(element){
+        console.log("element.value: ", element.value);
+        console.log("element: ", element)
+  
+        element.value = this.value;
+  
+        console.log("element.value: ", element.value);
+      } else {
+        console.error(`Element with id '${this.id}' not found.`);
+    }
+    }
+  }
+
+
+
 // Function to check if an element with a specific ID exists
 function elementExists(id) {
   return document.getElementById(id) !== null;
+}
+
+// Function to handle the "Read" button click
+function handleReadButtonClick() {
+  console.log('Read button clicked');
+  
+  let textString = new StringElement("field-Name", "Monster Name");
+
+  console.log("TextString: ", textString);
+
+  textString.readValue(); 
+
+  console.log(textString.name, ": ", textString.value);
+
+}
+
+// Function to handle the "Write" button click
+function handleWriteButtonClick() {
+  console.log('Write button clicked');
+  
+  let textString = new StringElement("field-Name", "Monster Name");
+
+  textString.value = "Not A Monster"
+
+  textString.writeValue();
 }
 
 // Create the "Read" button if it doesn't exist
@@ -11,6 +103,9 @@ if (!elementExists('readButton')) {
   readButton.style.backgroundColor = 'red';
   readButton.style.marginLeft = '10px'; // Adjust the margin as needed
 
+  // Add an event listener for the "Read" button
+  readButton.addEventListener('click', handleReadButtonClick);
+
   // Create the "Write" button if it doesn't exist
   if (!elementExists('writeButton')) {
     const writeButton = document.createElement('button');
@@ -18,6 +113,9 @@ if (!elementExists('readButton')) {
     writeButton.textContent = 'Write';
     writeButton.style.backgroundColor = 'red';
     writeButton.style.marginLeft = '10px'; // Adjust the margin as needed
+
+    // Add an event listener for the "Write" button
+    writeButton.addEventListener('click', handleWriteButtonClick);
 
     // Find the element with the class "page-title"
     const pageTitleElement = document.querySelector('.page-title');
@@ -31,10 +129,10 @@ if (!elementExists('readButton')) {
       console.error('Element with class "page-title" not found.');
     }
   } else {
-    console.error('Element with id "writeButton" already exists.');
+    console.log('Element with id "writeButton" already exists.');
   }
 } else {
-  console.error('Element with id "readButton" already exists.');
+  console.log('Element with id "readButton" already exists.');
 }
 
 
--- Extention/background.js
+++ Extention/background.js
@@ -31,7 +31,8 @@
 //   });
 // }
 
-// Function to check if the URL is a valid homebrew URL
+
+
 function isHomebrewURL(url) {
   return url.startsWith('https://www.dndbeyond.com/homebrew') && url.endsWith('edit');
 }
@@ -46,42 +47,73 @@ function injectScript(tabId) {
   });
 }
 
-function atemptInjection(tab){
-  if (tab.url) {
+// Function to check if the buttons exist in the current tab's HTML
+function buttonsExistInHTML(callback) {
+  // Query information about the currently active tab in the current window
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // Check if the tabs array is not empty, if the first tab has an ID, and if it has a valid URL
+    if (tabs && tabs[0] && tabs[0].id && tabs[0].url && tabs[0].url.startsWith('http')) {
+      // Execute a script in the context of the active tab
+      chrome.scripting.executeScript({
+        // Specify the target tab using its ID
+        target: { tabId: tabs[0].id },
+        // Define the script to be executed
+        func: function () {
+          // Check if the buttons exist in the HTML of the active tab
+          var readButton = document.getElementById('readButton');
+          var writeButton = document.getElementById('writeButton');
+          
+          // Return whether both buttons exist
+          return readButton !== null && writeButton !== null;
+        }
+      }, function (result) {
+        // Pass the result of the script execution to the callback function
+        callback(result);
+      });
+    } else {
+      // If there is no active tab or it has no ID or URL, or if the URL is not valid, indicate that buttons don't exist
+      callback(false);
+    }
+  });
+}
+
+function attemptInjection(tab) {
+  if (tab && tab.url) {
     // Check if the URL is a valid homebrew URL
     if (isHomebrewURL(tab.url)) {
       // Inject 'injection.js' into the active tab
       injectScript(tab.id);
+    } else {
+      console.log("Not a valid homebrew URL:", tab.url);
     }
   } else {
-    console.log("tab.url not present: ", tab);
+    console.log("Tab or tab.url does not exist.");
   }
 }
 
 // Event listener for the extension icon click
 chrome.action.onClicked.addListener((tab) => {
   console.log('Icon Clicked. Tab:', tab);
-
-  atemptInjection(tab);
-  
+  attemptInjection(tab);
 });
 
 // Event listener for tab updates (including page reloads)
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, updatedTab) => {
   if (changeInfo.status === "complete" && updatedTab) {
-    atemptInjection(updatedTab);
+    attemptInjection(updatedTab);
   } else {
     console.log("Waiting for tab completion.");
   }
 });
 
-chrome.tabs.onActivated.addListener((activeInfo) => {
-  chrome.tabs.get(activeInfo.tabId, (tab) => {
+// Event listener for web navigation completed (including page reloads)
+chrome.webNavigation.onCompleted.addListener((details) => {
+  console.log('Web Navigation Completed. Tab ID:', details.tabId);
+  chrome.tabs.get(details.tabId, (tab) => {
     if (tab) {
-      console.log("registered tab activation");
-      atemptInjection(tab);
+      attemptInjection(tab);
     } else {
       console.log("Tab not found.");
     }
   });
-});
+}, { url: [{ urlContains: 'https://www.dndbeyond.com/homebrew' }] });
