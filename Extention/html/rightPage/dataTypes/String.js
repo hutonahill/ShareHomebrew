41e282fbdd102fe3022925c7e82ff3682c776b5d
hutonahill <eriker@byui.edu>
2023-10-20 21:26:30 -0600

    started work on data dataTypes

---
+++ Extention/html/rightPage/dataTypes/String.js
@@ -0,0 +1,11 @@
+import Element from "Element.js";
+
+/**
+ * Represents a specialized Element for handling string values.
+ *
+ * @class
+ * @extends Element
+ */
+class String extends Element {
+
+}
